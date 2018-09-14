7a6251f97c6d1f744577a0a20bf66974a87c7a33
Maximiliano Jose Fernandez Wortman <maxifwortman@gmail.com>
2018-09-14 10:06:50 -0300

    first commit

---
+++ src/app/car-edit/car-edit.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit,OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarService } from '../shared/car/car.service';
+import { GiphyService } from '../shared/giphy/giphy.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-car-edit',
+  templateUrl: './car-edit.component.html',
+  styleUrls: ['./car-edit.component.css']
+})
+export class CarEditComponent implements OnInit,OnDestroy {
+  
+  car: any = {};
+
+  sub: Subscription;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private carService: CarService,
+    private giphyService: GiphyService) {
+}
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      const id = params['id'];
+      if (id) {
+        this.carService.get(id).subscribe((car: any) => {
+          if (car) {
+            this.car = car;
+            this.car.href = car._links.self.href;
+            this.giphyService.get(car.name).subscribe(url => car.giphyUrl = url);
+          } else {
+            console.log(`Car with id '${id}' not found, returning to list`);
+            this.gotoList();
+          }
+        });
+      }
+    });
+  }
+
+  save(form: NgForm) {
+    this.carService.save(form).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
+
+  remove(href) {
+    this.carService.remove(href).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
+  
+
+  gotoList() {
+    this.router.navigate(['/car-list']);
+  }
+
+}
